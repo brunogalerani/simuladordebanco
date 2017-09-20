c815a8e4e16426b9b24caed7eee330302462d0b8
brunogalerani <bruno_galerani@hotmail.com>
2017-09-20 20:30:55 -0300

    Tela final

--- app/src/main/java/app/com/sistemadebanco/FinalVeiculoActivity.java
+++ app/src/main/java/app/com/sistemadebanco/FinalVeiculoActivity.java
@@ -1,7 +1,9 @@
 package app.com.sistemadebanco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class FinalVeiculoActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class FinalVeiculoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_veiculo);
+
+        Intent intent = getIntent();
+        double valor = intent.getDoubleExtra("VALOR", 0);
+        Toast.makeText(this, String.valueOf(valor), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/app/com/sistemadebanco/SimuladorVeiculoActivity.java
+++ app/src/main/java/app/com/sistemadebanco/SimuladorVeiculoActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class SimuladorVeiculoActivity extends AppCompatActivity {
 
+    private RadioGroup radioGroup;
+    private EditText editTextValor;
+    private EditText editTextPorcentagem;
+    private EditText editTextParcelas;
+    private EditText editTextRendaMensal;
     private Button buttonCalcular;
 
     @Override
@@ -15,15 +23,45 @@ public class SimuladorVeiculoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulador_veiculo);
 
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupVeiculos);
+        editTextValor = (EditText) findViewById(R.id.editTextValorVeiculo);
+        editTextPorcentagem = (EditText) findViewById(R.id.editTextEntradaVeiculo);
+        editTextParcelas = (EditText) findViewById(R.id.editTextQuantidadeDeParcelasVeiculo);
+        editTextRendaMensal = (EditText) findViewById(R.id.editTextRendaMensalVeiculo);
         buttonCalcular = (Button) findViewById(R.id.buttonCalcularVeiculo);
 
         buttonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SimuladorVeiculoActivity.this, FinalVeiculoActivity.class);
-                startActivity(intent);
-                finish();
+                if (validaCampos()) {
+                    boolean isNovo = radioGroup.getCheckedRadioButtonId() == R.id.radioButtonVeiculoNovo;
+                    double valor = Double.parseDouble(editTextValor.getText().toString());
+                    double porcentagem = Double.parseDouble(editTextPorcentagem.getText().toString());
+                    int parcelas = Integer.parseInt(editTextParcelas.getText().toString());
+                    double rendaMensal = Double.parseDouble(editTextRendaMensal.getText().toString());
+
+                    Intent intent = new Intent(SimuladorVeiculoActivity.this, FinalVeiculoActivity.class);
+                    intent.putExtra("IS_NOVO", isNovo);
+                    intent.putExtra("VALOR", valor);
+                    intent.putExtra("PORCENTAGEM", porcentagem);
+                    intent.putExtra("PARCELAS", parcelas);
+                    intent.putExtra("RENDA_MENSAL", rendaMensal);
+
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(SimuladorVeiculoActivity.this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private boolean validaCampos() {
+        return !(
+                radioGroup.getCheckedRadioButtonId() == -1 ||
+                        editTextValor.getText().toString().isEmpty() ||
+                        editTextPorcentagem.getText().toString().isEmpty() ||
+                        editTextParcelas.getText().toString().isEmpty() ||
+                        editTextRendaMensal.getText().toString().isEmpty()
+        );
     }
 }
